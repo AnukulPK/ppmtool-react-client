c4ba1e484d992f6ffca51905a0bbeb71dc4f747d
unknown <anukulpk2@gmail.com>
2022-11-14 11:02:55 +0530

    additional components

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,13 @@
 import React from "react";
+import ProjectItem from "./Project/ProjectItem";
 
 const Dashboard = () => {
-  return <h1>Welcome to the Dashboard</h1>;
+  return (
+    <>
+      <h1>Welcome to the Dashboard</h1>
+      <ProjectItem />
+    </>
+  );
 };
 
 export default Dashboard;
---
+++ src/components/Project/ProjectItem.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProjectItem = () => {
+  return <h3>ProjectItem</h3>;
+};
+
+export default ProjectItem;
