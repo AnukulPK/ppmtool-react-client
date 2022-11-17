4f36fda6915f630d5ebbf95eee818cd21f3a298a
unknown <anukulpk2@gmail.com>
2022-11-17 11:55:21 +0530

    routing added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import AddProject from "./components/Project/AddProject";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/addProject" element={<AddProject />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
