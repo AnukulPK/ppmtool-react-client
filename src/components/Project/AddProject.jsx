136e3184c0709e0570a381c116492c2376426fc1
unknown <anukulpk2@gmail.com>
2022-11-18 12:31:14 +0530

    form Data added

--- src/components/Project/AddProject.jsx
+++ src/components/Project/AddProject.jsx
@@ -1,7 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddProject = () => {
-  return <div>AddProject</div>;
+  const [formData, setFormData] = useState({
+    projectName: "",
+    projectIdentifier: "",
+    description: "",
+    start_date: "",
+    end_date: "",
+  });
+
+  const { projectName, projectIdentifier, description, start_date, end_date } =
+    formData;
+
+  const onChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    const newProject = {
+      projectName: projectName,
+      projectIdentifier: projectIdentifier,
+      description: description,
+      start_date: start_date,
+      end_date: end_date,
+    };
+    console.log("formData", newProject);
+  };
+
+  return (
+    <div className="project">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 m-auto">
+            <h5 className="display-4 text-center">Create Project form</h5>
+            <hr />
+            <form className="form-display" onSubmit={onSubmitHandler}>
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control form-control-lg "
+                  placeholder="Project Name"
+                  name="projectName"
+                  onChange={(e) => onChange(e)}
+                  value={projectName}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control form-control-lg"
+                  placeholder="Unique Project ID"
+                  name="projectIdentifier"
+                  onChange={(e) => onChange(e)}
+                  value={projectIdentifier}
+                />
+              </div>
+
+              <div className="form-group">
+                <textarea
+                  className="form-control form-control-lg"
+                  placeholder="Project Description"
+                  name="description"
+                  onChange={(e) => onChange(e)}
+                  value={description}
+                ></textarea>
+              </div>
+              <h6>Start Date</h6>
+              <div className="form-group">
+                <input
+                  type="date"
+                  className="form-control form-control-lg"
+                  name="start_date"
+                  onChange={(e) => onChange(e)}
+                  value={start_date}
+                />
+              </div>
+              <h6>Estimated End Date</h6>
+              <div className="form-group">
+                <input
+                  type="date"
+                  className="form-control form-control-lg"
+                  name="end_date"
+                  onChange={(e) => onChange(e)}
+                  value={end_date}
+                />
+              </div>
+
+              <input type="submit" className="btn btn-primary btn-block mt-4" />
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AddProject;
