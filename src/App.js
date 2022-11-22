dac21fc5fe07947bb07a74d4809fdd456cea877d
unknown <anukulpk2@gmail.com>
2022-11-22 11:30:07 +0530

    redux initial dev

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import Header from "./components/Layout/Header";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import AddProject from "./components/Project/AddProject";
+import { Provider } from "react-redux";
+import { store } from "./redux/Store";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/addProject" element={<AddProject />} />
-        </Routes>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/addProject" element={<AddProject />} />
+          </Routes>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
---
+++ src/redux/features/todo/toDoSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const todoSlice = createSlice({
+  name: "todos",
+  initialState: {},
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: "123",
+        text: action.payload,
+      };
+
+      return [...state, todo];
+    },
+  },
+});
+
+export const { addTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
