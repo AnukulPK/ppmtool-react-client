10792ddd3a865cf239fc573683969696bcb64131
unknown <anukulpk2@gmail.com>
2022-12-14 18:55:05 +0530

    added validation

--- src/components/Project/UpdateProject.jsx
+++ src/components/Project/UpdateProject.jsx
@@ -16,18 +16,19 @@ const UpdateProject = () => {
     start_date: "",
     end_date: "",
   });
+
   const { projectName, projectIdentifier, description, start_date, end_date } =
     formData;
-  const { project, isError } = useSelector((state) => state.projects);
+  const { project, isError, message } = useSelector((state) => state.projects);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { projectId } = useParams();
 
   useEffect(() => {
-    if (isError) {
+    if (isError && message.projectIdentifier) {
       navigate("/dashboard");
     }
-  }, [isError, navigate]);
+  }, [isError, navigate, message.projectIdentifier]);
 
   useEffect(() => {
     dispatch(getProject(projectId));
@@ -76,12 +77,17 @@ const UpdateProject = () => {
               <div className="form-group">
                 <input
                   type="text"
-                  className="form-control form-control-lg "
+                  className={classNames("form-control form-control-lg", {
+                    "is-invalid": message.projectName,
+                  })}
                   placeholder="Project Name"
                   name="projectName"
                   value={projectName}
                   onChange={onChange}
                 />
+                {message.projectName && (
+                  <div className="invalid-feedback">{message.projectName}</div>
+                )}
               </div>
               <div className="form-group">
                 <input
@@ -97,32 +103,47 @@ const UpdateProject = () => {
 
               <div className="form-group">
                 <textarea
-                  className="form-control form-control-lg"
+                  className={classNames("form-control form-control-lg", {
+                    "is-invalid": message.description,
+                  })}
                   placeholder="Project Description"
                   value={description}
                   name="description"
                   onChange={onChange}
                 ></textarea>
+                {message.description && (
+                  <div className="invalid-feedback">{message.description}</div>
+                )}
               </div>
               <h6>Start Date</h6>
               <div className="form-group">
                 <input
                   type="date"
-                  className="form-control form-control-lg"
+                  className={classNames("form-control form-control-lg", {
+                    "is-invalid": message.start_date,
+                  })}
                   name="start_date"
                   value={start_date}
                   onChange={onChange}
                 />
+                {message.start_date && (
+                  <div className="invalid-feedback">{message.start_date}</div>
+                )}
               </div>
               <h6>Estimated End Date</h6>
               <div className="form-group">
                 <input
                   type="date"
-                  className="form-control form-control-lg"
+                  className={classNames("form-control form-control-lg", {
+                    "is-invalid": message.end_date,
+                  })}
                   name="end_date"
                   value={end_date}
                   onChange={onChange}
                 />
+                {message.end_date && (
+                  <div className="invalid-feedback">{message.end_date}</div>
+                )}
               </div>
 
               <input type="submit" className="btn btn-primary btn-block mt-4" />
