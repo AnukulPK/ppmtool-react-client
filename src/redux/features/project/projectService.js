e3a09b8caed711b4392e212da9c6ce1e9669b300
unknown <anukulpk2@gmail.com>
2022-12-23 14:55:41 +0530

    code refactor

--- src/redux/features/project/projectService.js
+++ src/redux/features/project/projectService.js
@@ -1,25 +1,25 @@
 import axios from "axios";
 
 const createProject = async (project, history) => {
-  await axios.post("http://localhost:8080/api/project", project);
+  await axios.post("/api/project", project);
   history("/dashboard");
 };
 
 const getProjects = async () => {
-  const response = await axios.get("http://localhost:8080/api/project/all");
+  const response = await axios.get("/api/project/all");
   return response.data;
 };
 
 const getProject = async (projectId) => {
-  const response = await axios.get(
-    `http://localhost:8080/api/project/${projectId}`
-  );
+  const response = await axios.get(`/api/project/${projectId}`);
   return response.data;
 };
 
 const deleteProject = async (projectId) => {
-  await axios.delete(`http://localhost:8080/api/project/${projectId}`);
-  return projectId;
+  if (window.confirm("Are you sure? This will delete the project")) {
+    await axios.delete(`/api/project/${projectId}`);
+    return projectId;
+  }
 };
 
 const projectService = {
