fa4ad1434bae344ea0f687fc4b0e920afd06b7b3
unknown <anukulpk2@gmail.com>
2023-03-06 13:31:47 +0530

    update project structure

---
+++ src/components/ProjectBoard/ProjectTasks/AddProjectTask.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddProjectTask = () => {
+  return <div>AddProjectTask</div>;
+};
+
+export default AddProjectTask;
---
+++ src/components/ProjectBoard/ProjectTasks/UpdateProjectTask.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UpdateProjectTask = () => {
+  return <div>UpdateProjectTask</div>;
+};
+
+export default UpdateProjectTask;
